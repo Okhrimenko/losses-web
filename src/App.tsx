192e3c0bccc9c8de0964a1db8661d398b3932a67
Andrey Okhrimenko <AOkhrimenko@allvuesystems.com>
2023-08-05 16:32:32 +0300

    init-commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, FunctionComponent } from 'react';
+import axios from 'axios';
+import {  
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  
+  ResponsiveContainer,
+  AreaChart,
+  Area
+} from "recharts";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const endPoint = 'https://lossesdataprocessor.azurewebsites.net/api/GetData?code=JmpmzExq9HsNu5Sv4Wzseb6jJeFX5NSOPztjTPbvxsusAzFuy5iZsA==';
+
+interface LossModel {
+  Total: number;
+  Date: Date;
 }
+
+
+const App : FunctionComponent = React.memo(() => {
+  const [data, setDate] = React.useState<Array<LossModel>>([])
+
+  useEffect(() => {
+
+    axios.get(endPoint).then(response=>{
+      setDate(response.data)
+    });
+    
+  }, []);
+  
+  return(
+    <div className="App">
+      <header  className="App-header">
+       <h1>Documenting Russian Equipment Losses During The Russian Invasion Of Ukraine</h1>
+       <div style={{ width: '90%', height: 500 }}>
+        <ResponsiveContainer>
+          <AreaChart
+            data={data}
+            margin={{
+              top: 10,
+              right: 30,
+              left: 0,
+              bottom: 0,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="Date" />
+            <YAxis  type="number" domain={[0, 20000]}/>
+            <Tooltip />
+            <Area type="monotone" dataKey="Total" stroke="#8884d8" fill="#8884d8" />
+          </AreaChart>
+        </ResponsiveContainer>
+      </div>
+        </header>
+    
+    </div>
+  )
+});
 
 export default App;
