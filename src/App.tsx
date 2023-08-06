0b92ce5d6d40a5131c150b73dfe9ab63258136ed
Andrey Okhrimenko <AOkhrimenko@allvuesystems.com>
2023-08-06 13:42:52 +0300

    New charts

--- src/App.tsx
+++ src/App.tsx
@@ -36,16 +36,9 @@ const App : FunctionComponent = React.memo(() => {
       <header  className="App-header">
        <h1>Documenting Russian Equipment Losses During The Russian Invasion Of Ukraine</h1>
        <div style={{ width: '90%', height: 500 }}>
+        <div>Total count of <b>Losses</b> vehicles and equipment</div>
         <ResponsiveContainer>
-          <AreaChart
-            data={data}
-            margin={{
-              top: 10,
-              right: 30,
-              left: 0,
-              bottom: 0,
-            }}
-          >
+          <AreaChart data={data} margin={{ top: 10, right: 30, left: 0, bottom: 0}}>
             <CartesianGrid strokeDasharray="3 3" />
             <XAxis dataKey="Date" />
             <YAxis  type="number" domain={[0, 20000]}/>
@@ -54,6 +47,33 @@ const App : FunctionComponent = React.memo(() => {
           </AreaChart>
         </ResponsiveContainer>
       </div>
+
+      <div style={{ width: '90%', height: 500, marginTop:50 }}>
+        <div>Total count of <b>Captured</b> vehicles and equipment</div>
+        <ResponsiveContainer>
+          <AreaChart data={data} margin={{ top: 10, right: 30, left: 0, bottom: 0}}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="Date" />
+            <YAxis  type="number" domain={[0, 20000]}/>
+            <Tooltip />
+            <Area type="monotone" dataKey="Captured" stroke="#8884d8" fill="#8884d8" />
+          </AreaChart>
+        </ResponsiveContainer>
+      </div>
+
+<div style={{ width: '90%', height: 500, marginTop:50 }}>
+        <div>Total count of <b>Destroyed</b> vehicles and equipment</div>
+        <ResponsiveContainer>
+          <AreaChart data={data} margin={{ top: 10, right: 30, left: 0, bottom: 0}}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="Date" />
+            <YAxis  type="number" domain={[0, 20000]}/>
+            <Tooltip />
+            <Area type="monotone" dataKey="Destroyed" stroke="#8884d8" fill="#8884d8" />
+          </AreaChart>
+        </ResponsiveContainer>
+      </div>
+
         </header>
     
     </div>
