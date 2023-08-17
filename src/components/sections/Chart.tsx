f46d603e46c020962c32be29abe18eed314a0e25
Andrey Okhrimenko <AOkhrimenko@allvuesystems.com>
2023-08-17 16:55:55 +0300

    Added DateRange filter for Chart

--- src/components/sections/Chart.tsx
+++ src/components/sections/Chart.tsx
@@ -9,8 +9,12 @@ import {
   Area,
   Legend,
 } from "recharts";
-import { Paper, Typography } from "@mui/material";
+import { Paper, Stack, Typography, Button } from "@mui/material";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import { ILossModel } from "../../interfaces";
+import dayjs, { Dayjs } from "dayjs";
 
 interface IPoprs {
   data: Array<ILossModel>;
@@ -18,15 +22,80 @@ interface IPoprs {
 
 const Chart: FunctionComponent<IPoprs> = React.memo(({ data }) => {
   const [maxTotal] = React.useState<number>(15000);
+  const [startDate, setStartDate] = React.useState<Dayjs | null>(null);
+  const [endDate, setEndDate] = React.useState<Dayjs | null>(null);
+
+  const maxDate = React.useMemo(() => {
+    return data.length > 0
+      ? data.reduce((a, b) => (a.Date > b.Date ? a : b))
+      : null;
+  }, [data]);
+
+  const minDate = React.useMemo(() => {
+    return data.length > 0
+      ? data.reduce((a, b) => (a.Date < b.Date ? a : b))
+      : null;
+  }, [data]);
+
+  const clearFilters = () => {
+    setStartDate(null);
+    setEndDate(null);
+  };
+
+  const filtered = React.useMemo(() => {
+    let result = [...data];
+
+    if (startDate) {
+      result = [...result.filter((o) => dayjs(o.Date) >= dayjs(startDate))];
+    }
+
+    if (endDate) {
+      result = [...result.filter((o) => dayjs(o.Date) <= dayjs(endDate))];
+    }
+
+    return result;
+  }, [data, startDate, endDate]);
 
   return (
     <Paper>
-      <div style={{ height: 550, marginTop: 20 }}>
-        <Typography variant="h6">
-          Total count of <b>Losses</b> vehicles and equipment
-        </Typography>
+      <div style={{ height: 500, marginTop: 20 }}>
+        <Stack sx={{ p: 1 }} direction="row" spacing={3}>
+          <Typography variant="h6" alignContent={"center"}>
+            Total count of <b>Losses</b> vehicles and equipment
+          </Typography>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker
+              minDate={dayjs(minDate?.Date)}
+              maxDate={endDate}
+              label="Start Date"
+              value={startDate}
+              onChange={(newValue) => {
+                setStartDate(newValue);
+              }}
+            />
+
+            <DatePicker
+              label="End Date"
+              value={endDate}
+              minDate={startDate}
+              maxDate={dayjs(maxDate?.Date)}
+              onChange={(newValue) => {
+                setEndDate(newValue);
+              }}
+            />
+
+            <Button
+              variant="contained"
+              color="info"
+              onClick={() => clearFilters()}
+            >
+              {"x"}
+            </Button>
+          </LocalizationProvider>
+        </Stack>
+
         <ResponsiveContainer>
-          <AreaChart data={data}>
+          <AreaChart data={filtered}>
             <CartesianGrid strokeDasharray="3 3" />
             <XAxis dataKey="Date" />
             <YAxis type="number" domain={[0, maxTotal]} />
